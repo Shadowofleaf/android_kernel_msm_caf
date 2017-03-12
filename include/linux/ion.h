de2e1f8226d40a9a674e5c61b4ad7b012d807029
simokb96 <simokb46@gmail.com>
2017-03-12 15:39:29 +0000

    Revert "Revert "Revert "ion: ion mini upgrade"""
    This reverts commit 3da02e88763526cc58b4caafde4cd8fd7343449a.

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -51,15 +51,6 @@ enum ion_heap_type {
 #define ION_HEAP_CARVEOUT_MASK		(1 << ION_HEAP_TYPE_CARVEOUT)
 #define ION_HEAP_TYPE_DMA_MASK         (1 << ION_HEAP_TYPE_DMA)
 
-/**
- * heap flags - the lower 16 bits are used by core ion, the upper 16
- * bits are reserved for use by the heaps themselves.
- */
-#define ION_FLAG_CACHED 1    /* mappings of this buffer should be
-             cached, ion will do cache
-             maintenance when the buffer is
-             mapped for dma */
-
 
 /**
  * heap flags - the lower 16 bits are used by core ion, the upper 16
@@ -520,14 +511,6 @@ static inline int msm_ion_do_cache_op(struct ion_client *client,
 struct ion_allocation_data {
 	size_t len;
 	size_t align;
-        unsigned int heap_mask;
-	unsigned int flags;
-	struct ion_handle *handle;
-};
-
-struct ion_allocation_data_compat {
-	size_t len;
-	size_t align;
 	unsigned int flags;
 	struct ion_handle *handle;
 };
@@ -580,9 +563,6 @@ struct ion_custom_data {
 #define ION_IOC_ALLOC_COMPAT	_IOWR(ION_IOC_MAGIC, 0, \
 				      struct ion_allocation_data_old)
 
-#define ION_IOC_ALLOC_COMPAT _IOWR(ION_IOC_MAGIC, 0, \
-				      struct ion_allocation_data_compat)
-
 /**
  * DOC: ION_IOC_FREE - free memory
  *
@@ -620,8 +600,6 @@ struct ion_custom_data {
  */
 #define ION_IOC_IMPORT		_IOWR(ION_IOC_MAGIC, 5, struct ion_fd_data)
 
-#define ION_IOC_IMPORT_COMPAT	_IOWR(ION_IOC_MAGIC, 5, int)
-
 /**
  * DOC: ION_IOC_CUSTOM - call architecture specific ion ioctl
  *
@@ -639,6 +617,8 @@ struct ion_custom_data {
  *
  * Clean the caches of the handle specified.
  */
+#define ION_IOC_CLEAN_CACHES  _IOWR(ION_IOC_MAGIC, 20, \
+						struct ion_flush_data)
 #define ION_IOC_CLEAN_CACHES_COMPAT	_IOWR(ION_IOC_MAGIC, 7, \
 						struct ion_flush_data)
 
@@ -647,18 +627,15 @@ struct ion_custom_data {
  *
  * Invalidate the caches of the handle specified.
  */
-#define ION_IOC_INV_CACHES_COMPAT	_IOWR(ION_IOC_MAGIC, 8, \
+#define ION_IOC_INV_CACHES  _IOWR(ION_IOC_MAGIC, 21, \
 						struct ion_flush_data)
 /**
  * DOC: ION_MSM_IOC_CLEAN_CACHES - clean and invalidate the caches
  *
  * Clean and invalidate the caches of the handle specified.
  */
-#define ION_IOC_CLEAN_INV_CACHES_COMPAT	_IOWR(ION_IOC_MAGIC, 9, \
+#define ION_IOC_CLEAN_INV_CACHES  _IOWR(ION_IOC_MAGIC, 22, \
 						struct ion_flush_data)
-
-#define ION_IOC_CLEAN_INV_CACHES_COMPAT	_IOWR(ION_IOC_MAGIC, 9, \
-					    struct ion_flush_data)
 
 /**
  * DOC: ION_IOC_GET_FLAGS - get the flags of the handle
@@ -666,11 +643,12 @@ struct ion_custom_data {
  * Gets the flags of the current handle which indicate cachability,
  * secure state etc.
  */
-#define ION_IOC_GET_FLAGS_COMPAT	_IOWR(ION_IOC_MAGIC, 10, \
+#define ION_IOC_GET_FLAGS    _IOWR(ION_IOC_MAGIC, 23, \
 						struct ion_flag_data)
 
 #define ION_IOC_GET_FLAGS_COMPAT _IOWR(ION_IOC_MAGIC, 10, \
               struct ion_flag_data)
+
 
 /**
  * DOC: ION_IOC_SYNC - BOGUS
